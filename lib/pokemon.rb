3cd04ea1bae301ac2320c618a5879fc9e5a7ba77
EPICZEUS <s77broz@aol.com>
2018-11-07 16:06:11 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+	def self.find(id, db)
+		data = db.execute("SELECT * FROM pokemon WHERE id = ?", id).first
+
+		self.new(id: data[0], name: data[1], type: data[2], db: db)
+	end
+
+	def self.save(name, type, db)
+		db.execute("INSERT OR IGNORE INTO pokemon(name, type) VALUES (?, ?)", name, type)
+	end
+
+	attr_accessor :name, :type
+	attr_reader :id, :db
+
+	def initialize(id:, name:, type:, db:)
+		@id = id
+		@name = name
+		@type = type
+		@db = db
+
+		db.execute("INSERT OR IGNORE INTO pokemon(id, name, type) VALUES (?, ?, ?)", id, name, type)
+	end
 end
