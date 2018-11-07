f918b5d5e0ddfba7e989e15728fa926534259b28
EPICZEUS <s77broz@aol.com>
2018-11-07 16:50:25 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,14 +10,21 @@ class Pokemon
 	end
 
 	attr_accessor :name, :type
-	attr_reader :id, :db
+	attr_reader :id, :db, :hp
 
 	def initialize(id:, name:, type:, db:)
 		@id = id
 		@name = name
 		@type = type
 		@db = db
+		@hp = 60
 
 		db.execute("INSERT OR IGNORE INTO pokemon(id, name, type) VALUES (?, ?, ?)", id, name, type)
 	end
+
+	# def alter_hp(hp, db)
+	# 	@hp = hp
+
+	# 	db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, @id)
+	# end
 end
